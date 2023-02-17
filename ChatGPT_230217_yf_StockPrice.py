1ebdc8b238d57a86ad8f8b08d265ce5999a1c3eb
thinknew59 <thinknew59@gmail.com>
2023-02-17 16:54:08 +0900

    yfinance에서 애플 주식 가격 가져오기를 ChatGPT가 짜 준 것

--- ChatGPT_230217_yf_StockPrice.py
+++ ChatGPT_230217_yf_StockPrice.py
@@ -1,29 +1,44 @@
-import requests
+# import requests
 import yfinance as yf
 
-# Yahoo Finance API URL
-url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary"
+# # Yahoo Finance API URL
+# url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary"
+#
+# # Query parameters
+# querystring = {"symbol":"AAPL"}
+#
+# # Headers
+# headers = {
+#     "X-RapidAPI-Key": "YOUR_RAPIDAPI_KEY",
+#     "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
+# }
+#
+# # Make a request to the Yahoo Finance API
+# response = requests.request("GET", url, headers=headers, params=querystring)
+#
+# # Parse the JSON response and get the current price of the stock
+# data = response.json()
+# current_price = data["price"]["regularMarketPrice"]["raw"]
+#
+# # Use yfinance library to get historical prices of the stock
+# apple_stock = yf.Ticker("AAPL")
+# historical_prices = apple_stock.history(period="max")
+#
+# # Print the current price and the historical prices
+# print("Current price of AAPL: ${:.2f}".format(current_price))
+# print(historical_prices)
 
-# Query parameters
-querystring = {"symbol":"AAPL"}
+import yfinance as yf
 
-# Headers
-headers = {
-    "X-RapidAPI-Key": "YOUR_RAPIDAPI_KEY",
-    "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
-}
+# Ticker symbol for Apple stock
+ticker_symbol = "AAPL"
 
-# Make a request to the Yahoo Finance API
-response = requests.request("GET", url, headers=headers, params=querystring)
+# Period for which we want to download the historical data
+# In this case, we are downloading the data for the last 5 years
+period = "5y"
 
-# Parse the JSON response and get the current price of the stock
-data = response.json()
-current_price = data["price"]["regularMarketPrice"]["raw"]
+# Download the historical data for the given period
+apple_stock = yf.download(ticker_symbol, period=period)
 
-# Use yfinance library to get historical prices of the stock
-apple_stock = yf.Ticker("AAPL")
-historical_prices = apple_stock.history(period="max")
-
-# Print the current price and the historical prices
-print("Current price of AAPL: ${:.2f}".format(current_price))
-print(historical_prices)
+# Print the historical data
+print(apple_stock)
